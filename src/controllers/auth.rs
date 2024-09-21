ed4fbc35fa99d34cb52f55e9fb4b729ba6b1e96e
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-09-21 15:37:52 +0000

    add: ユーザー認証から投稿一覧への遷移を追加

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,16 +1,61 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
+use crate::{
+    models::_entities::{
+        self,
+        users::{ActiveModel, Column},
+    },
+    views,
+};
 use axum::debug_handler;
-use loco_rs::prelude::*;
+use format::redirect;
+use loco_rs::{db::create, prelude::*};
+use sea_orm::{sea_query::Order, QueryOrder};
+use serde::{Deserialize, Serialize};
 
-use crate::views;
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Params {
+    pub display_name: String,
+    pub user_id: String,
+}
+
+impl Params {
+    fn update(&self, item: &mut ActiveModel) {
+        item.display_name = Set(self.display_name.clone());
+        item.user_id = Set(self.user_id.clone());
+    }
+}
 
 #[debug_handler]
 pub async fn input(ViewEngine(v): ViewEngine<TeraView>) -> Result<Response> {
     views::auth::input(&v)
 }
 
+#[debug_handler]
+pub async fn add(
+    ViewEngine(v): ViewEngine<TeraView>,
+    State(ctx): State<AppContext>,
+    Json(params): Json<Params>,
+) -> Result<Response> {
+    let mut item = ActiveModel {
+        icon_path: Set(None),
+        ..Default::default()
+    };
+    params.update(&mut item);
+    item.insert(&ctx.db).await?;
+
+    let item = _entities::posts::Entity::find()
+        .order_by(Column::Id, Order::Desc)
+        .all(&ctx.db)
+        .await?;
+
+    views::auth::add(&v, &item).await
+}
+
 pub fn routes() -> Routes {
-    Routes::new().prefix("auths").add("/", get(input))
+    Routes::new()
+        .prefix("auths/")
+        .add("/", get(input))
+        .add("/", post(add))
 }
--- src/views/auth.rs
+++ src/views/auth.rs
@@ -1,5 +1,7 @@
 use loco_rs::prelude::*;
 
+use crate::models::_entities::posts;
+
 /// Render a list view of auths.
 ///
 /// # Errors
@@ -7,4 +9,13 @@ use loco_rs::prelude::*;
 /// When there is an issue with rendering the view.
 pub fn input(v: &impl ViewRenderer) -> Result<Response> {
     format::render().view(v, "auth/input.html", serde_json::json!({}))
+}
+
+/// Redirect to the list view of posts.
+///
+/// # Errors
+///
+/// When there is an issue with rendering the view.
+pub async fn add(v: &impl ViewRenderer, items: &Vec<posts::Model>) -> Result<Response> {
+    format::render().view(v, "post/list.html", serde_json::json!({"items": items}))
 }
